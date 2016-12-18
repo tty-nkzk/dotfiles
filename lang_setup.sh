73b3d1bda112cb88c7d0a72813e7b74a62d4aac2
tty-nkzk <tty.nkzk@gmail.com>
2016-12-18 17:44:14 +0900

    init

---
+++ lang_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export LANG=ja_JP.utf-8
+export LC_ALL=$LANG
+
+if [ ! ${EUID:-${UID}} = 0 ]; then
+	echo 'sorry..';
+	echo 'I am not root.';
+	exit 0;
+fi
+
+apt-get install -y language-pack-ja
+dpkg-reconfigure tzdata
+dpkg-reconfigure locales
+update-locale LANG=$LANG
